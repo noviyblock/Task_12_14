2593d08479ea80accd0fbdb12a178f32bab54184
noviyblock <noviy_block@mail.ru>
2022-12-18 15:30:42 +0300

    start-files

---
+++ src/ru/vsu/cs/new_block/task_12_14/console_app/ConsoleApp.java
@@ -0,0 +1,4 @@
+package ru.vsu.cs.new_block.task_12_14.console_app;
+
+public class ConsoleApp {
+}
--- src/ru/vsu/cs/new_block/task_12_14/swing_app/Main.java
+++ src/ru/vsu/cs/new_block/task_12_14/swing_app/Main.java
@@ -1,3 +1,5 @@
+package ru.vsu.cs.new_block.task_12_14.swing_app;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
